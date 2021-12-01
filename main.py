3e663ca72c9e932dffa989cf6409d3878597a22d
Steven Vandegrift <svandegrift@icloud.com>
2021-12-01 00:10:32 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# Steven Vandegrift 2021.11.30
+# Day 1 🎉 Band Name Generator 🥁
+
+print("Welcome to the band name generator!")
+city = input("What is the name of the city you grew up in?\n")
+pet_name = input("What is the name of your pet?\n")
+band_name = city + "'s " + pet_name
+print("Your band name is " + band_name)
+
